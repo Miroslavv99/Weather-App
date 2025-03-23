5d2c0aafcfc3eac58932db8d2d3329a151d8b3b3
Miroslav <mbochko99@gmail.com>
2025-03-23 23:37:04 +0200

    feat: Create WeatherService class to handle fetching weather data from API

--- src/components/WeatherService.js
+++ src/components/WeatherService.js
@@ -1,10 +1,19 @@
-export async function getWeather(url) {
-  try {
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log(data);
-    return data;
-  } catch (error) {
-    console.error(error);
+export class WeatherService {
+  constructor(apiKey) {
+    this.apiKey = apiKey;
+  }
+  async getApiData(url) {
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`An error has occurred: ${response.status}`);
+      }
+      const data = await response.json();
+      console.log(data);
+      return data;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
   }
 }
