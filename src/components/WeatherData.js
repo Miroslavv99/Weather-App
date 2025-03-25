9af5a2c7b3515180b44e8cacaaf706d85023a16f
Miroslav <mbochko99@gmail.com>
2025-03-25 09:59:07 +0200

    feat: Added class for creating weekly objects

--- src/components/WeatherData.js
+++ src/components/WeatherData.js
@@ -2,8 +2,17 @@ export class WeatherData {
   constructor(city, country, icon, conditions, temperature, description) {
     this.city = city;
     this.country = country;
-    this.conditions = conditions;
     this.icon = icon;
+    this.conditions = conditions;
+    this.temperature = temperature;
+    this.description = description;
+  }
+}
+
+export class WeeklyData {
+  constructor(icon, conditions, temperature, description) {
+    this.icon = icon;
+    this.conditions = conditions;
     this.temperature = temperature;
     this.description = description;
   }
