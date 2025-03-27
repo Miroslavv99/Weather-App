8fa3ec1d7d92bc30d500fa130daf4d92cd0cc6f0
Miroslav <mbochko99@gmail.com>
2025-03-27 16:47:55 +0200

    refactor: File paths have been changed

--- src/components/IconSelector.js
+++ src/components/IconSelector.js
@@ -5,44 +5,45 @@ export class SelectIcon {
 
     switch (icon) {
       case "partly-cloudy-night":
-        weatherImg.src = "/images/cloudy-night.svg";
+        weatherImg.src = "images/cloudy-night.svg";
         break;
       case "partly-cloudy-day":
-        weatherImg.src = "/images/partly-cloudy.svg";
+        weatherImg.src = "images/partly-cloudy.svg";
         break;
 
       case "cloudy":
       case "wind":
-        weatherImg.src = "/images/cloudy.svg";
+        weatherImg.src = "images/cloudy.svg";
         break;
 
       case "rain":
       case "showers-day":
-        weatherImg.src = "/images/rainy.svg";
+        weatherImg.src = "images/rainy.svg";
         break;
 
       case "showers-night":
-        weatherImg.src = "/images/rainy.svg";
+        weatherImg.src = "images/rainy.svg";
+        break;
 
       case "fog":
-        weatherImg.src = "/images/fog.svg";
+        weatherImg.src = "images/fog.svg";
         break;
 
       case "snow":
       case "snow-showers-day":
       case "snow-showers-night":
-        weatherImg.src = "/images/snow.svg";
+        weatherImg.src = "images/snow.svg";
         break;
 
       case "thunder-rain":
       case "thunder-showers-day":
       case "thunder-showers-night":
-        weatherImg.src = "/images/thunderstorm.svg";
+        weatherImg.src = "images/thunderstorm.svg";
         break;
 
       case "clear-day":
       case "clear-night":
-        weatherImg.src = "/images/sunny.svg";
+        weatherImg.src = "images/sunny.svg";
         break;
     }
     return weatherImg;
